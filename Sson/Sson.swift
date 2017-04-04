0db41c324e0a9fa77b44a4cd60ff21bb04c0d146
권성욱(Sungwook Kwon)/OCB개발팀/SKP <baw0803@sk.com>
2017-04-04 16:51:58 +0900

    First Commit

---
+++ Sson/Sson.swift
@@ -0,0 +1,121 @@
+//
+//  Sson.swift
+//  NetTest
+//
+//  Created by baw0803 on 2017. 1. 18..
+//  Copyright © 2017년 WookyNim. All rights reserved.
+//
+
+import Foundation
+
+class Sson : NSObject {
+    
+    private let cfBundleName = "CFBundleName"
+    private let cMirrorForStr = "Mirror for "
+    private let cToken = "<>"
+    private let cEmptyStr = ""
+    
+    func fromJson(_ jsonData:Any, classNm:AnyClass) -> Any {
+        
+        let dicData = jsonData as! NSDictionary
+        
+        let classIns = classNm as! NSObject.Type
+        let dynamicClass = classIns.init()
+        
+        let reflection = Mirror(reflecting: dynamicClass)
+        
+        if let superClassMirror = reflection.superclassMirror {
+            superClassMirror.children.forEach {
+                if let value = dicData[$0.label!] {
+                    dynamicClass.setValue(value, forKey: $0.label!)
+                }
+            }
+        }
+    
+        reflection.children.filter { $0.label != nil }.forEach {
+            let memberName = $0.label!
+            
+            
+            let valueReflection = Mirror(reflecting: $0.value)
+            
+            
+            if let pValue = dicData[memberName] {
+                
+                if self.isObjectType(valueReflection.subjectType) {
+                    dynamicClass.setValue(pValue, forKey: memberName)
+                }
+                else {
+                    let strArray = valueReflection.description.components(separatedBy: CharacterSet(charactersIn : cToken)).filter { $0.characters.count > 0}.map{ $0.contains(cMirrorForStr) ? $0.replacingOccurrences(of: cMirrorForStr, with: cEmptyStr) : $0 }.filter { self.isContainStrings($0) }
+                    
+                    if strArray.count == 1 {
+                        let className = self.classNameParsing(strArray[0])
+                        if let pInstance = NSClassFromString(className) as? NSObject.Type {
+                            let value = self.fromJson(pValue, classNm: pInstance)
+                            dynamicClass.setValue(value, forKey: memberName)
+                        }
+                    }
+                    else if strArray.count > 1{
+                        if let pArray = pValue as? NSArray {
+                            var newArray = Array<AnyObject>()
+                            pArray.forEach {
+                                let className = self.classNameParsing(strArray[1])
+                                if let pInstance = NSClassFromString(className) as? NSObject.Type {
+                                    let value = self.fromJson($0, classNm: pInstance)
+                                    newArray.append(value as AnyObject)
+                                }
+                            }
+                            dynamicClass.setValue(newArray, forKey: memberName)
+                        }
+                        
+                    }
+                }
+                
+            }
+        }
+        
+        return dynamicClass
+    }
+    
+    func toJson(_ insClass:AnyObject) -> Any {
+        
+        let dicData = NSMutableDictionary()
+        
+        let reflection = Mirror(reflecting: insClass)
+        
+        reflection.children.forEach {
+            let memberName = $0.label!
+            
+            if let pValue = insClass.value(forKey: memberName){
+                let className = self.classNameParsing(memberName)
+                if let _ = NSClassFromString(className) as? NSObject.Type {
+                    let recursiveIns = self.toJson(pValue as AnyObject)
+                    dicData[memberName] = recursiveIns
+                }
+                else {
+                    dicData[memberName] = pValue
+                }
+            }
+        }
+        
+        return dicData
+    }
+    
+    private func isContainStrings(_ str:String) -> Bool{
+        if !(str.contains("Optional") || str.contains("String") || str.contains("NSNumber") || str.contains("NSArray") || str.contains("NSDictionary")) {
+            return true
+        }
+        return false
+    }
+    
+    private func isObjectType(_ type: Any.Type) -> Bool{
+        let isContain = type == String.self || type == String?.self || type == NSNumber.self || type == NSNumber?.self || type == NSArray.self || type == NSArray?.self || type == NSDictionary.self || type == NSDictionary?.self ? true : false
+        return isContain
+    }
+    
+    private func classNameParsing(_ fullName : String) -> String {
+        let pjName = Bundle.main.object(forInfoDictionaryKey: cfBundleName) as! String
+        return pjName.replacingOccurrences(of: " ", with: "_")+"."+fullName
+    }
+    
+}
+
---
+++ Sson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sson
+//
+//  Created by wooy83 on 2017. 2. 8..
+//  Copyright © 2017년 wooky83. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
